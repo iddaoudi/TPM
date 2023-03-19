adfbdbeededf658fb2b902a3a5e5917647a276a3
Idriss Daoudi <idaoudi@anl.gov>
2023-03-19 18:48:11 +0100

    [feature] better counter management

---
+++ algorithms/dense/counters.h
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  counters.h
+ *
+ *    Description:  PAPI counters struct
+ *
+ *        Version:  1.0
+ *        Created:  19/03/2023
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:  Idriss Daoudi <idaoudi@anl.gov>
+ *   Organization:  Argonne National Laboratory
+ *
+ * =====================================================================================
+ */
+
+typedef struct {
+  long long values[NEVENTS];
+  double arithm_intensity;
+  double mem_boundness;
+  double bmr;
+  double ilp;
+} CounterData;
+
+void compute_derived_metrics(CounterData *data) {
+  data->arithm_intensity = (double)data->values[0] / (double)data->values[1];
+  data->mem_boundness = (double)data->values[2] / (double)data->values[3];
+  data->bmr = (double)data->values[4] / (double)data->values[5];
+  data->ilp = (double)data->values[1] / (double)data->values[3];
+}
+
+void accumulate_counters(long long dst[], long long src[][NEVENTS], int available_threads) {
+  memset(dst, 0, NEVENTS * sizeof(long long));
+  for (int i = 0; i < NEVENTS; i++) {
+    for (int j = 0; j < available_threads; j++) {
+      dst[i] += src[j][i];
+    }
+  }
+}
--- algorithms/dense/qr.h
+++ algorithms/dense/qr.h
@@ -18,6 +18,8 @@
 
 #define NEVENTS 6
 
+#include "counters.h"
+
 void qr(tpm_desc A, tpm_desc S)
 {
   char *name_with_id_char = NULL;
@@ -26,7 +28,7 @@ void qr(tpm_desc A, tpm_desc S)
   int eventset = PAPI_NULL;
   long long values[NEVENTS];
   const int available_threads = omp_get_max_threads();
-  
+
   long long(*values_by_thread_geqrt)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
   long long(*values_by_thread_ormqr)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
   long long(*values_by_thread_tsqrt)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
@@ -344,57 +346,23 @@ void qr(tpm_desc A, tpm_desc S)
       }
     }
   }
-    
-#pragma omp taskwait
-  PAPI_shutdown();
-
-  long long total_values_geqrt[NEVENTS];
-  long long total_values_ormqr[NEVENTS];
-  long long total_values_tsqrt[NEVENTS];
-  long long total_values_tsmqr[NEVENTS];
-
-  memset(total_values_geqrt, 0, sizeof(total_values_geqrt));
-  memset(total_values_ormqr, 0, sizeof(total_values_ormqr));
-  memset(total_values_tsqrt, 0, sizeof(total_values_tsqrt));
-  memset(total_values_tsmqr, 0, sizeof(total_values_tsmqr));
-
-  for (int i = 0; i < NEVENTS; i++)
-  {
-    for (int j = 0; j < available_threads; j++)
-    {
-      total_values_geqrt[i] += values_by_thread_geqrt[j][i];
-      total_values_ormqr[i] += values_by_thread_ormqr[j][i];
-      total_values_tsqrt[i] += values_by_thread_tsqrt[j][i];
-      total_values_tsmqr[i] += values_by_thread_tsmqr[j][i];
-    }
-  }
 
   if (TPM_PAPI)
   {
-    // PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS
-    // Arithmetic intensity
-    double arithm_int_geqrt = (double)(total_values_geqrt[0]) / (double)(total_values_geqrt[1]);
-    double arithm_int_ormqr = (double)(total_values_ormqr[0]) / (double)(total_values_ormqr[1]);
-    double arithm_int_tsqrt = (double)(total_values_tsqrt[0]) / (double)(total_values_tsqrt[1]);
-    double arithm_int_tsmqr = (double)(total_values_tsmqr[0]) / (double)(total_values_tsmqr[1]);
+#pragma omp taskwait
+    PAPI_shutdown();
 
-    // Memory boundness
-    double mem_boundness_geqrt = (double)(total_values_geqrt[2]) / (double)(total_values_geqrt[3]);
-    double mem_boundness_ormqr = (double)(total_values_ormqr[2]) / (double)(total_values_ormqr[3]);
-    double mem_boundness_tsqrt = (double)(total_values_tsqrt[2]) / (double)(total_values_tsqrt[3]);
-    double mem_boundness_tsmqr = (double)(total_values_tsmqr[2]) / (double)(total_values_tsmqr[3]);
+    CounterData geqrt, ormqr, tsqrt, tsmqr;
 
-    // BMR
-    double bmr_geqrt = (double)(total_values_geqrt[4]) / (double)(total_values_geqrt[5]);
-    double bmr_ormqr = (double)(total_values_ormqr[4]) / (double)(total_values_ormqr[5]);
-    double bmr_tsqrt = (double)(total_values_tsqrt[4]) / (double)(total_values_tsqrt[5]);
-    double bmr_tsmqr = (double)(total_values_tsmqr[4]) / (double)(total_values_tsmqr[5]);
+    accumulate_counters(geqrt.values, values_by_thread_geqrt, available_threads);
+    accumulate_counters(ormqr.values, values_by_thread_ormqr, available_threads);
+    accumulate_counters(tsqrt.values, values_by_thread_tsqrt, available_threads);
+    accumulate_counters(tsmqr.values, values_by_thread_tsmqr, available_threads);
 
-    // ILP
-    double ilp_geqrt = (double)(total_values_geqrt[1]) / (double)(total_values_geqrt[3]);
-    double ilp_ormqr = (double)(total_values_ormqr[1]) / (double)(total_values_ormqr[3]);
-    double ilp_tsqrt = (double)(total_values_tsqrt[1]) / (double)(total_values_tsqrt[3]);
-    double ilp_tsmqr = (double)(total_values_tsmqr[1]) / (double)(total_values_tsmqr[3]);
+    compute_derived_metrics(&geqrt);
+    compute_derived_metrics(&ormqr);
+    compute_derived_metrics(&tsqrt);
+    compute_derived_metrics(&tsmqr);
 
     FILE *file;
     if ((file = fopen("counters.dat", "w")) == NULL)
@@ -404,10 +372,14 @@ void qr(tpm_desc A, tpm_desc S)
     }
     else
     {
-      fprintf(file, "geqrt, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size, mem_boundness_geqrt, arithm_int_geqrt, bmr_geqrt, ilp_geqrt, total_values_geqrt[0]);
-      fprintf(file, "ormqr, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size, mem_boundness_ormqr, arithm_int_ormqr, bmr_ormqr, ilp_ormqr, total_values_ormqr[0]);
-      fprintf(file, "tsqrt, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size, mem_boundness_tsqrt, arithm_int_tsqrt, bmr_tsqrt, ilp_tsqrt, total_values_tsqrt[0]);
-      fprintf(file, "tsmqr, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size, mem_boundness_tsmqr, arithm_int_tsmqr, bmr_tsmqr, ilp_tsmqr, total_values_tsmqr[0]);
+      fprintf(file, "geqrt, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              geqrt.mem_boundness, geqrt.arithm_intensity, geqrt.bmr, geqrt.ilp, geqrt.values[0]);
+      fprintf(file, "ormqr, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              ormqr.mem_boundness, ormqr.arithm_intensity, ormqr.bmr, ormqr.ilp, ormqr.values[0]);
+      fprintf(file, "tsqrt, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              tsqrt.mem_boundness, tsqrt.arithm_intensity, tsqrt.bmr, tsqrt.ilp, tsqrt.values[0]);
+      fprintf(file, "tsmqr, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              tsmqr.mem_boundness, tsmqr.arithm_intensity, tsmqr.bmr, tsmqr.ilp, tsmqr.values[0]);
 
       fclose(file);
     }
