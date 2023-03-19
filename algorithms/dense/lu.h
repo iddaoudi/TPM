76242b88573e8a000957c2129746fd465c495233
Idriss Daoudi <idaoudi@anl.gov>
2023-03-19 19:14:16 +0100

    [feature] same implementation of counters for LU

--- algorithms/dense/lu.h
+++ algorithms/dense/lu.h
@@ -18,6 +18,28 @@
 
 void lu(tpm_desc A)
 {
+  char *name_with_id_char = NULL;
+  struct timeval start, end;
+
+  int eventset = PAPI_NULL;
+  long long values[NEVENTS];
+  const int available_threads = omp_get_max_threads();
+
+  long long(*values_by_thread_getrf)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
+  long long(*values_by_thread_trsm1)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
+  long long(*values_by_thread_trsm2)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
+  long long(*values_by_thread_gemm)[NEVENTS] = malloc(available_threads * sizeof(long long[NEVENTS]));
+
+  if (TPM_PAPI)
+  {
+    int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
+
+    memset(values_by_thread_getrf, 0, available_threads * sizeof(long long[NEVENTS]));
+    memset(values_by_thread_trsm1, 0, available_threads * sizeof(long long[NEVENTS]));
+    memset(values_by_thread_trsm2, 0, available_threads * sizeof(long long[NEVENTS]));
+    memset(values_by_thread_gemm, 0, available_threads * sizeof(long long[NEVENTS]));
+  }
+
   // TPM library: initialization
   if (TPM_TRACE)
     tpm_downstream_start("lu", A.matrix_size, A.tile_size, NTH);
@@ -28,29 +50,66 @@ void lu(tpm_desc A)
   {
     double *tileA = A(k, k);
 
-    // TPM library: create a unique task name
-    char *name_with_id_char = tpm_unique_task_identifier("getrf", k, m, n);
     if (TPM_TRACE)
+    {
+      // TPM library: create a unique task name
+      name_with_id_char = tpm_unique_task_identifier("getrf", k, m, n);
       tpm_upstream_set_task_name(name_with_id_char);
+    }
 
 #pragma omp task depend(inout \
                         : tileA[0])
     {
-      // TPM library: send CPU and name
-      struct timeval start, end;
-      unsigned int cpu, node;
-      getcpu(&cpu, &node);
-      if (TPM_TRACE)
+      if (TPM_PAPI)
+      {
+        memset(values, 0, sizeof(values));
+        eventset = PAPI_NULL;
+        int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
+        int ret = PAPI_create_eventset(&eventset);
+        if (ret != PAPI_OK)
+        {
+          printf("GETRF task - PAPI_create_eventset error %d: %s\n", ret, PAPI_strerror(ret));
+          exit(1);
+        }
+        PAPI_add_events(eventset, events, NEVENTS);
+
+        // Start PAPI counters
+        PAPI_start(eventset);
+      }
+      else if (TPM_TRACE)
+      {
+        // TPM library: send CPU and name
+        unsigned int cpu, node;
+        getcpu(&cpu, &node);
+
         tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
 
-      // Kernel
-      gettimeofday(&start, NULL);
-      tpm_dgetrf(A.tile_size, tileA, A.tile_size);
-      gettimeofday(&end, NULL);
+        gettimeofday(&start, NULL);
+      }
 
-      // TPM library: send time and name
-      if (TPM_TRACE)
+      // Kernel
+      tpm_dgetrf(A.tile_size, tileA, A.tile_size);
+
+      if (TPM_PAPI)
+      {
+        // Start PAPI counters
+        PAPI_stop(eventset, values);
+
+        // Accumulate events values
+        for (int i = 0; i < NEVENTS; i++)
+        {
+#pragma omp atomic update
+          values_by_thread_getrf[omp_get_thread_num()][i] += values[i];
+        }
+        PAPI_unregister_thread();
+      }
+      else if (TPM_TRACE)
+      {
+        gettimeofday(&end, NULL);
+
+        // TPM library: send time and name
         tpm_upstream_get_task_time(start, end, name_with_id_char);
+      }
     }
 
     for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
@@ -58,33 +117,70 @@ void lu(tpm_desc A)
       double *tileA = A(k, k);
       double *tileB = A(m, k);
 
-      // TPM library: create a unique task name
-      char *name_with_id_char = tpm_unique_task_identifier("trsm", k, m, n);
       if (TPM_TRACE)
+      {
+        // TPM library: create a unique task name
+        name_with_id_char = tpm_unique_task_identifier("trsm1", k, m, n);
         tpm_upstream_set_task_name(name_with_id_char);
+      }
 
 #pragma omp task depend(in                                     \
                         : tileA [0:A.tile_size * A.tile_size]) \
     depend(inout                                               \
            : tileB[A.tile_size * A.tile_size])
       {
-        // TPM library: send CPU and name
-        struct timeval start, end;
-        unsigned int cpu, node;
-        getcpu(&cpu, &node);
-        if (TPM_TRACE)
+        if (TPM_PAPI)
+        {
+          memset(values, 0, sizeof(values));
+          eventset = PAPI_NULL;
+          int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
+          int ret = PAPI_create_eventset(&eventset);
+          if (ret != PAPI_OK)
+          {
+            printf("TRSM1 task - PAPI_create_eventset error %d: %s\n", ret, PAPI_strerror(ret));
+            exit(1);
+          }
+          PAPI_add_events(eventset, events, NEVENTS);
+
+          // Start PAPI counters
+          PAPI_start(eventset);
+        }
+        else if (TPM_TRACE)
+        {
+          // TPM library: send CPU and name
+          unsigned int cpu, node;
+          getcpu(&cpu, &node);
+
           tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
 
+          gettimeofday(&start, NULL);
+        }
+
         // Kernel
-        gettimeofday(&start, NULL);
         cblas_dtrsm(CblasColMajor, CblasRight, CblasUpper, CblasNoTrans,
                     CblasNonUnit, A.tile_size, A.tile_size, 1.0, tileA,
                     A.tile_size, tileB, A.tile_size);
-        gettimeofday(&end, NULL);
 
-        // TPM library: send time and name
-        if (TPM_TRACE)
+        if (TPM_PAPI)
+        {
+          // Start PAPI counters
+          PAPI_stop(eventset, values);
+
+          // Accumulate events values
+          for (int i = 0; i < NEVENTS; i++)
+          {
+#pragma omp atomic update
+            values_by_thread_trsm1[omp_get_thread_num()][i] += values[i];
+          }
+          PAPI_unregister_thread();
+        }
+        else if (TPM_TRACE)
+        {
+          gettimeofday(&end, NULL);
+
+          // TPM library: send time and name
           tpm_upstream_get_task_time(start, end, name_with_id_char);
+        }
       }
     }
 
@@ -93,33 +189,70 @@ void lu(tpm_desc A)
       double *tileA = A(k, k);
       double *tileB = A(k, n);
 
-      // TPM library: create a unique task name
-      char *name_with_id_char = tpm_unique_task_identifier("trsm", k, m, n);
       if (TPM_TRACE)
+      {
+        // TPM library: create a unique task name
+        name_with_id_char = tpm_unique_task_identifier("trsm2", k, m, n);
         tpm_upstream_set_task_name(name_with_id_char);
+      }
 
 #pragma omp task depend(in                                     \
                         : tileA [0:A.tile_size * A.tile_size]) \
     depend(inout                                               \
            : tileB[A.tile_size * A.tile_size])
       {
-        // TPM library: send CPU and name
-        struct timeval start, end;
-        unsigned int cpu, node;
-        getcpu(&cpu, &node);
-        if (TPM_TRACE)
+        if (TPM_PAPI)
+        {
+          memset(values, 0, sizeof(values));
+          eventset = PAPI_NULL;
+          int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
+          int ret = PAPI_create_eventset(&eventset);
+          if (ret != PAPI_OK)
+          {
+            printf("TRSM2 task - PAPI_create_eventset error %d: %s\n", ret, PAPI_strerror(ret));
+            exit(1);
+          }
+          PAPI_add_events(eventset, events, NEVENTS);
+
+          // Start PAPI counters
+          PAPI_start(eventset);
+        }
+        else if (TPM_TRACE)
+        {
+          // TPM library: send CPU and name
+          unsigned int cpu, node;
+          getcpu(&cpu, &node);
+
           tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
 
+          gettimeofday(&start, NULL);
+        }
+
         // Kernel
-        gettimeofday(&start, NULL);
         cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans,
                     CblasUnit, A.tile_size, A.tile_size, 1.0, tileA,
                     A.tile_size, tileB, A.tile_size);
-        gettimeofday(&end, NULL);
 
-        // TPM library: send time and name
-        if (TPM_TRACE)
+        if (TPM_PAPI)
+        {
+          // Start PAPI counters
+          PAPI_stop(eventset, values);
+
+          // Accumulate events values
+          for (int i = 0; i < NEVENTS; i++)
+          {
+#pragma omp atomic update
+            values_by_thread_trsm2[omp_get_thread_num()][i] += values[i];
+          }
+          PAPI_unregister_thread();
+        }
+        else if (TPM_TRACE)
+        {
+          gettimeofday(&end, NULL);
+
+          // TPM library: send time and name
           tpm_upstream_get_task_time(start, end, name_with_id_char);
+        }
       }
 
       for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
@@ -128,36 +261,117 @@ void lu(tpm_desc A)
         double *tileB = A(k, n);
         double *tileC = A(m, n);
 
-        // TPM library: create a unique task name
-        char *name_with_id_char = tpm_unique_task_identifier("gemm", k, m, n);
         if (TPM_TRACE)
+        {
+          // TPM library: create a unique task name
+          name_with_id_char = tpm_unique_task_identifier("gemm", k, m, n);
           tpm_upstream_set_task_name(name_with_id_char);
-          
+        }
+
 #pragma omp task depend(in                                     \
                         : tileA [0:A.tile_size * A.tile_size], \
                           tileB [0:A.tile_size * A.tile_size]) \
     depend(inout                                               \
            : tileC [0:A.tile_size * A.tile_size])
         {
-          // TPM library: send CPU and name
-          struct timeval start, end;
-          unsigned int cpu, node;
-          getcpu(&cpu, &node);
-          if (TPM_TRACE)
+          if (TPM_PAPI)
+          {
+            memset(values, 0, sizeof(values));
+            eventset = PAPI_NULL;
+            int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
+            int ret = PAPI_create_eventset(&eventset);
+            if (ret != PAPI_OK)
+            {
+              printf("GEMM task - PAPI_create_eventset error %d: %s\n", ret, PAPI_strerror(ret));
+              exit(1);
+            }
+            PAPI_add_events(eventset, events, NEVENTS);
+
+            // Start PAPI counters
+            PAPI_start(eventset);
+          }
+          else if (TPM_TRACE)
+          {
+            // TPM library: send CPU and name
+            unsigned int cpu, node;
+            getcpu(&cpu, &node);
+
             tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
 
+            gettimeofday(&start, NULL);
+          }
+
           // Kernel
-          gettimeofday(&start, NULL);
           cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, A.tile_size,
                       A.tile_size, A.tile_size, -1.0, tileA, A.tile_size, tileB,
                       A.tile_size, 1.0, tileC, A.tile_size);
-          gettimeofday(&end, NULL);
 
-          // TPM library: send time and name
-          if (TPM_TRACE)
+          if (TPM_PAPI)
+          {
+            // Start PAPI counters
+            PAPI_stop(eventset, values);
+
+            // Accumulate events values
+            for (int i = 0; i < NEVENTS; i++)
+            {
+#pragma omp atomic update
+              values_by_thread_gemm[omp_get_thread_num()][i] += values[i];
+            }
+            PAPI_unregister_thread();
+          }
+          else if (TPM_TRACE)
+          {
+            gettimeofday(&end, NULL);
+
+            // TPM library: send time and name
             tpm_upstream_get_task_time(start, end, name_with_id_char);
+          }
         }
       }
     }
   }
+
+  if (TPM_PAPI)
+  {
+#pragma omp taskwait
+    PAPI_shutdown();
+
+    CounterData getrf, trsm1, trsm2, gemm;
+
+    accumulate_counters(getrf.values, values_by_thread_getrf, available_threads);
+    accumulate_counters(trsm1.values, values_by_thread_trsm1, available_threads);
+    accumulate_counters(trsm2.values, values_by_thread_trsm2, available_threads);
+    accumulate_counters(gemm.values, values_by_thread_gemm, available_threads);
+
+    compute_derived_metrics(&getrf);
+    compute_derived_metrics(&trsm1);
+    compute_derived_metrics(&trsm2);
+    compute_derived_metrics(&gemm);
+
+    FILE *file;
+    if ((file = fopen("counters.dat", "w")) == NULL)
+    {
+      perror("fopen failed");
+      exit(1);
+    }
+    else
+    {
+      fprintf(file, "getrf, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              getrf.mem_boundness, getrf.arithm_intensity, getrf.bmr, getrf.ilp, getrf.values[0]);
+      fprintf(file, "trsm1, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              trsm1.mem_boundness, trsm1.arithm_intensity, trsm1.bmr, trsm1.ilp, trsm1.values[0]);
+      fprintf(file, "trsm2, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              trsm2.mem_boundness, trsm2.arithm_intensity, trsm2.bmr, trsm2.ilp, trsm2.values[0]);
+      fprintf(file, "gemm, %d, %d, %f, %f, %f, %f, %lld\n", A.matrix_size, A.tile_size,
+              gemm.mem_boundness, gemm.arithm_intensity, gemm.bmr, gemm.ilp, gemm.values[0]);
+
+      fclose(file);
+    }
+  }
+
+  free(values_by_thread_getrf);
+  free(values_by_thread_trsm1);
+  free(values_by_thread_trsm2);
+  free(values_by_thread_gemm);
+
 }
