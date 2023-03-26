2947e61ac7d35070c6f1012866d1d9abdd7297b6
Idriss Daoudi <idaoudi@anl.gov>
2023-03-26 18:25:18 +0200

    [exp] reorganizing and few improvements to python scripts

--- data/manipulate_data.py
+++ data/manipulate_data.py
@@ -19,19 +19,10 @@
 import pandas as pd
 import sys
 
-# Counters file first, energy second
-def concatenate_files():
-    file1 = pd.read_csv(sys.argv[1], skipinitialspace=True)
-    file2 = pd.read_csv(sys.argv[2], skipinitialspace=True)
-    merged_file = pd.merge(
-        file1, file2, on=["algorithm", "matrix_size", "tile_size"])
-    merged_file.to_csv("merged_file_cholesky.csv", index=False)
-    return 0
-
 def mean_of_files():
     # Read the CSV files into separate dataframes
     dfs = []
-    for file in sys.argv[1:]:
+    for file in sys.argv[2:]:
         df = pd.read_csv(file, sep='\s*,\s*', engine='python')
         dfs.append(df)
     # Concatenate the dataframes vertically
@@ -39,17 +30,23 @@ def mean_of_files():
     # Group the data by algorithm, matrix_size, tile_size, and case, and calculate the mean of PKG1, PKG2, DRAM1, DRAM2, and time
     grouped_df = merged_df.groupby(['algorithm', 'matrix_size', 'tile_size', 'case']).mean().reset_index()
     # Write the result to a new CSV file
-    grouped_df.to_csv('energy_data_cholesky_mean.csv', index=False)
+    grouped_df.to_csv('energy_data_qr_mean_32768.csv', index=False)
 
-    return 0
+# Counters file first, energy second
+def concatenate_files():
+    file1 = pd.read_csv(sys.argv[2], skipinitialspace=True)
+    file2 = pd.read_csv(sys.argv[3], skipinitialspace=True)
+    merged_file = pd.merge(
+        file1, file2, on=["algorithm", "matrix_size", "tile_size"])
+    merged_file.to_csv("merged_file_qr.csv", index=False)
 
 def get_best_cases():
-    data = pd.read_csv(sys.argv[1])
+    data = pd.read_csv(sys.argv[2])
     data["total_energy"] = data[["PKG1", "PKG2", "DRAM1", "DRAM2"]].sum(axis=1)
     best_cases = data.groupby(['matrix_size', 'tile_size']).apply(
         lambda x: x.loc[
             (x["total_energy"] < x.loc[x["case"] == 1, "total_energy"].values[0]) &
-            (x["time"] <= x.loc[x["case"] == 1, "time"].values[0] * 1.15)
+            (x["time"] <= x.loc[x["case"] == 1, "time"].values[0] * 1.05)
         ]
     ).reset_index(drop=True)
     for matrix_size in best_cases['matrix_size'].unique():
@@ -61,15 +58,11 @@ def get_best_cases():
             print()
 
 if __name__ == '__main__':
-    mean = 0
-    concatenate = 0
-    best_cases = 1
+    key = int(sys.argv[1])
 
-    if (mean):
+    if (key == 1):
         ret = mean_of_files()
-        assert (ret == 0)
-    if (concatenate):
+    if (key == 2):
         ret = concatenate_files()
-        #assert (ret == 0)
-    if (best_cases):
+    if (key == 3):
         get_best_cases()
--- data/plot.py
+++ data/plot.py
@@ -21,14 +21,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-df = pd.read_csv(sys.argv[1])
-
-# Group the data by algorithm, matrix_size, and tile_size
-groups = df.groupby(['algorithm', 'matrix_size', 'tile_size'])
-
-# Define a list of colors to use for the marker gradient
-colors = np.linspace(0, 1, num=16)
-
 marker_symbols = {
     1: 'o',
     2: 's',
@@ -48,34 +40,108 @@ marker_symbols = {
     16: 'X',
 }
 
-# Iterate over each group and create a separate graph for each one
-for name, group in groups:
-    # Get the values for the horizontal and vertical lines
-    case1 = group.loc[group['case'] == 1]
-    x_val = case1['time'].iloc[0] * 1.05
-    y_val = case1[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum(axis=1).iloc[0]
+def plot_function():
+    # Iterate over each group and create a separate graph for each one
+    for name, group in groups:
+        # Get the values for the horizontal and vertical lines
+        case1 = group.loc[group['case'] == 1]
+        x_val = case1['time'].iloc[0] * 1.15
+        y_val = case1[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum(axis=1).iloc[0]
     
-    # Create a scatter plot with time on the x-axis and sum of PKG1,PKG2,DRAM1,DRAM2 on the y-axis
-    fig, ax = plt.subplots()
-    handles = []
-    for i, row in group.iterrows():
-        marker_color = plt.cm.jet(colors[row['case'] - 1])
-        handle = ax.scatter(row['time'], row[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum(), color=marker_color, marker=marker_symbols[row['case']], label='c' + str(row['case']), s=95, alpha=0.9)
-        handles.append(handle)
+        # Create a scatter plot with time on the x-axis and sum of PKG1,PKG2,DRAM1,DRAM2 on the y-axis
+        fig, ax = plt.subplots()
+        handles = []
+        for i, row in group.iterrows():
+            marker_color = plt.cm.jet(colors[row['case'] - 1])
+            handle = ax.scatter(row['time'], row[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum(), color=marker_color, marker=marker_symbols[row['case']], label='c' + str(row['case']), s=95, alpha=0.9)
+            handles.append(handle)
     
-    # Add the vertical and horizontal lines with legends
-    ax.axvline(x_val, color='r', linestyle='--', label=f'DET + 15% ({x_val:.2f})')
-    ax.axhline(y_val, color='r', linestyle='--', label=f'DEC ({y_val:.2f})')
+        # Add the vertical and horizontal lines with legends
+        ax.axvline(x_val, color='r', linestyle='--', label=f'DET + 15% ({x_val:.2f})')
+        ax.axhline(y_val, color='r', linestyle='--', label=f'DEC ({y_val:.2f})')
     
-    # Set the title and axis labels
-    plt.title(f'{name[0]}, {name[1]}, {name[2]}')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Energy (uJ)')
-    plt.subplots_adjust(top=0.85, bottom=0.1)
+        # Set the title and axis labels
+        plt.title(f'{name[0]}, {name[1]}, {name[2]}')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Energy (uJ)')
+        plt.subplots_adjust(top=0.85, bottom=0.1)
 
-    # Simplify the legend to only show the case number
-    ax.legend(handles, [f'c{i}' for i in range(1, 17)], loc='center left', fancybox=True, shadow=True, bbox_to_anchor=(1, 0.5))
+        # Simplify the legend to only show the case number
+        ax.legend(handles, [f'c{i}' for i in range(1, 17)], loc='center left', fancybox=True, shadow=True, bbox_to_anchor=(1, 0.5))
     
-    # Save the figure and show the plot
-    plt.savefig(f'{name[0]}_{name[1]}_{name[2]}.png', bbox_inches='tight')
-    plt.show()
+        # Save the figure and show the plot
+        plt.savefig(f'function_{name[0]}_{name[1]}_{name[2]}.png', bbox_inches='tight')
+        plt.show()
+
+def plot_total():
+    for name, group in groups:
+        case1 = group.loc[group['case'] == 1]
+        y_val = case1[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum(axis=1).iloc[0]
+
+        fig, ax = plt.subplots()
+
+        x_values = []
+        y_values = []
+        for i, row in group.iterrows():
+            x_values.append(row['case'])
+            y_values.append(row[['PKG1', 'PKG2', 'DRAM1', 'DRAM2']].sum())
+
+        ax.plot(x_values, y_values, '-x', linewidth=3, color='red', label='Total energy')
+        ax.axhline(y_val, color='r', linestyle='--', label='Default total energy')
+
+        ax.legend()
+        ax.grid()
+
+        plt.title(f'{name[0]}, {name[1]}, {name[2]}')
+        plt.xlabel('Cases')
+        plt.ylabel('Energy consumption (uJ)')
+        plt.subplots_adjust(top=0.85, bottom=0.1)
+
+        plt.savefig(f'total_{name[0]}_{name[1]}_{name[2]}.png', bbox_inches='tight')
+        plt.show()
+
+def plot_sum():
+    for name, group in groups:
+        case1 = group.loc[group['case'] == 1]
+        y_val1 = case1[['PKG1', 'PKG2']].sum(axis=1).iloc[0]
+        y_val2 = case1[['DRAM1', 'DRAM2']].sum(axis=1).iloc[0]
+
+        fig, ax = plt.subplots()
+
+        x_values = []
+        y_values1 = []
+        y_values2 = []
+        for i, row in group.iterrows():
+            x_values.append(row['case'])
+            y_values1.append(row[['PKG1', 'PKG2']].sum())
+            y_values2.append(row[['DRAM1', 'DRAM2']].sum())
+
+        ax.plot(x_values, y_values1, '-x', linewidth=3, color='red', label='PKG')
+        ax.plot(x_values, y_values2, '-x', linewidth=3, color='blue', label='DRAM')
+        ax.axhline(y_val1, color='r', linestyle='--', label='Default PKG')
+        ax.axhline(y_val2, color='b', linestyle='--', label='Default DRAM')
+
+        ax.legend()
+        ax.grid()
+
+        plt.title(f'{name[0]}, {name[1]}, {name[2]}')
+        plt.xlabel('Cases')
+        plt.ylabel('Energy consumption (uJ)')
+        plt.subplots_adjust(top=0.85, bottom=0.1)
+
+        plt.savefig(f'sum_{name[0]}_{name[1]}_{name[2]}.png', bbox_inches='tight')
+        plt.show()
+
+if __name__ == '__main__':
+    key = int(sys.argv[1])
+    df = pd.read_csv(sys.argv[2])
+
+    groups = df.groupby(['algorithm', 'matrix_size', 'tile_size'])
+    colors = np.linspace(0, 1, num=16)
+
+    if key == 1:
+        plot_function()
+    elif key == 2:
+        plot_total()
+    elif key == 3:
+        plot_sum()
