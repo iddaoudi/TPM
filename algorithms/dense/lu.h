41146d8d42b6f3381fa3a227cabe31cb28e1b44a
Idriss Daoudi <idaoudi@anl.gov>
2023-05-15 20:58:31 +0200

    [feature] new LU algorithm with pivoting

--- algorithms/dense/lu.h
+++ algorithms/dense/lu.h
@@ -7,7 +7,7 @@
  *
  *        Version:  1.0
  *        Created:  25/12/2022
- *       Revision:  13/05/2023
+ *       Revision:  15/05/2023
  *       Compiler:  clang
  *
  *         Author:  Idriss Daoudi <idaoudi@anl.gov>
@@ -16,310 +16,82 @@
  * =====================================================================================
  */
 
-void lu(tpm_desc A)
+// #define LOG 1
+
+void lu(int matrix_size, int tile_size, double *pA, int *ipiv, double *A)
 {
-  char *name_with_id_char = NULL;
-  struct timeval start = (struct timeval){0};
-  struct timeval end = (struct timeval){0};
+    double alpha = 1., neg = -1.;
+    tpm_matrix_to_tile(A, pA, matrix_size, matrix_size, tile_size, matrix_size);
 
-  int eventset = PAPI_NULL;
-  long long values[NEVENTS];
-  const int available_threads = omp_get_max_threads();
+#ifdef LOG
+    tpm_default_print_matrix("A", pA, matrix_size);
+#endif
 
-  // NEVENTS + 1 for the task weights
-  CounterData *getrf = (CounterData *)malloc(available_threads * sizeof(CounterData));
-  CounterData *trsm1 = (CounterData *)malloc(available_threads * sizeof(CounterData));
-  CounterData *trsm2 = (CounterData *)malloc(available_threads * sizeof(CounterData));
-  CounterData *gemm = (CounterData *)malloc(available_threads * sizeof(CounterData));
-
-  if (TPM_PAPI)
-  {
-    int events[NEVENTS] = {PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS};
-    int ret = PAPI_create_eventset(&eventset);
-    PAPI_add_events(eventset, events, NEVENTS);
-
-    for (int i = 0; i < available_threads; i++)
+    for (int k = 0; k < matrix_size / tile_size; k++)
     {
-      memset(getrf[i].values, 0, (NEVENTS + 1) * sizeof(long long));
-      memset(trsm1[i].values, 0, (NEVENTS + 1) * sizeof(long long));
-      memset(trsm2[i].values, 0, (NEVENTS + 1) * sizeof(long long));
-      memset(gemm[i].values, 0, (NEVENTS + 1) * sizeof(long long));
-    }
-  }
+        int m = matrix_size - k * tile_size;
+        double *akk = A + k * tile_size * matrix_size + k * tile_size * tile_size;
 
-  // TPM library: initialization
-  if (TPM_TRACE)
-    tpm_downstream_start("lu", A.matrix_size, A.tile_size, NTH);
-
-  int k = 0, m = 0, n = 0;
-
-  for (k = 0; k < A.matrix_size / A.tile_size; k++)
-  {
-    double *tileA = A(k, k);
-
-    if (TPM_TRACE)
-    {
-      // TPM library: create a unique task name
-      name_with_id_char = tpm_unique_task_identifier("getrf", k, m, n);
-      tpm_upstream_set_task_name(name_with_id_char);
-    }
-
-#pragma omp task depend(inout : tileA[0])
-    {
-      if (TPM_PAPI)
-      {
-        memset(values, 0, sizeof(values));
-        // Start PAPI counters
-        int ret_start = PAPI_start(eventset);
-        if (ret_start != PAPI_OK)
+#pragma omp task depend(inout : akk[0 : m * tile_size]) \
+    depend(out : ipiv[k * tile_size : tile_size])       \
+    firstprivate(akk, m)
         {
-          printf("PAPI_start GETRF error %d: %s\n", ret_start, PAPI_strerror(ret_start));
-          exit(EXIT_FAILURE);
-        }
-      }
-      else if (TPM_TRACE)
-      {
-        // TPM library: send CPU and name
-        unsigned int cpu, node;
-        getcpu(&cpu, &node);
-        tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
-        gettimeofday(&start, NULL);
-      }
 
-      // Kernel
-      tpm_dgetrf(A.tile_size, tileA, A.tile_size);
-
-      if (TPM_PAPI)
-      {
-        // Start PAPI counters
-        int ret_stop = PAPI_stop(eventset, values);
-        if (ret_stop != PAPI_OK)
-        {
-          printf("PAPI_stop GETRF error %d: %s\n", ret_stop, PAPI_strerror(ret_stop));
-          exit(EXIT_FAILURE);
-        }
-        // Accumulate events values
-        for (int i = 0; i < NEVENTS; i++)
-        {
-          getrf[omp_get_thread_num()].values[i] += values[i];
-        }
-        getrf[omp_get_thread_num()].values[NEVENTS]++;
-      }
-      else if (TPM_TRACE)
-      {
-        gettimeofday(&end, NULL);
-        // TPM library: send time and name
-        tpm_upstream_get_task_time(start, end, name_with_id_char);
-      }
-    }
-
-    for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
-    {
-      double *tileA = A(k, k);
-      double *tileB = A(m, k);
-
-      if (TPM_TRACE)
-      {
-        // TPM library: create a unique task name
-        name_with_id_char = tpm_unique_task_identifier("trsm", k, m, n);
-        tpm_upstream_set_task_name(name_with_id_char);
-      }
-
-#pragma omp task depend(in : tileA[0 : A.tile_size * A.tile_size]) \
-    depend(inout : tileB[A.tile_size * A.tile_size])
-      {
-        if (TPM_PAPI)
-        {
-          memset(values, 0, sizeof(values));
-          // Start PAPI counters
-          int ret_start = PAPI_start(eventset);
-          if (ret_start != PAPI_OK)
-          {
-            printf("PAPI_start TRSM1 error %d: %s\n", ret_start, PAPI_strerror(ret_start));
-            exit(EXIT_FAILURE);
-          }
-        }
-        else if (TPM_TRACE)
-        {
-          // TPM library: send CPU and name
-          unsigned int cpu, node;
-          getcpu(&cpu, &node);
-          tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
-          gettimeofday(&start, NULL);
-        }
-
-        // Kernel
-        cblas_dtrsm(CblasColMajor, CblasRight, CblasUpper, CblasNoTrans,
-                    CblasNonUnit, A.tile_size, A.tile_size, 1.0, tileA,
-                    A.tile_size, tileB, A.tile_size);
-
-        if (TPM_PAPI)
-        {
-          // Start PAPI counters
-          int ret_stop = PAPI_stop(eventset, values);
-          if (ret_stop != PAPI_OK)
-          {
-            printf("PAPI_stop TRSM1 error %d: %s\n", ret_stop, PAPI_strerror(ret_stop));
-            exit(EXIT_FAILURE);
-          }
-          // Accumulate events values
-          for (int i = 0; i < NEVENTS; i++)
-          {
-            trsm1[omp_get_thread_num()].values[i] += values[i];
-          }
-          trsm1[omp_get_thread_num()].values[NEVENTS]++;
-        }
-        else if (TPM_TRACE)
-        {
-          gettimeofday(&end, NULL);
-          // TPM library: send time and name
-          tpm_upstream_get_task_time(start, end, name_with_id_char);
-        }
-      }
-    }
-
-    for (n = k + 1; n < A.matrix_size / A.tile_size; n++)
-    {
-      double *tileA = A(k, k);
-      double *tileB = A(k, n);
-
-      if (TPM_TRACE)
-      {
-        // TPM library: create a unique task name
-        name_with_id_char = tpm_unique_task_identifier("trsm", k, m, n);
-        tpm_upstream_set_task_name(name_with_id_char);
-      }
-
-#pragma omp task depend(in : tileA[0 : A.tile_size * A.tile_size]) \
-    depend(inout : tileB[A.tile_size * A.tile_size])
-      {
-        if (TPM_PAPI)
-        {
-          memset(values, 0, sizeof(values));
-          // Start PAPI counters
-          int ret_start = PAPI_start(eventset);
-          if (ret_start != PAPI_OK)
-          {
-            printf("PAPI_start TRSM2 error %d: %s\n", ret_start, PAPI_strerror(ret_start));
-            exit(EXIT_FAILURE);
-          }
-        }
-        else if (TPM_TRACE)
-        {
-          // TPM library: send CPU and name
-          unsigned int cpu, node;
-          getcpu(&cpu, &node);
-          tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
-          gettimeofday(&start, NULL);
-        }
-
-        // Kernel
-        cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans,
-                    CblasUnit, A.tile_size, A.tile_size, 1.0, tileA,
-                    A.tile_size, tileB, A.tile_size);
-
-        if (TPM_PAPI)
-        {
-          // Start PAPI counters
-          int ret_stop = PAPI_stop(eventset, values);
-          if (ret_stop != PAPI_OK)
-          {
-            printf("PAPI_stop TRSM2 error %d: %s\n", ret_stop, PAPI_strerror(ret_stop));
-            exit(EXIT_FAILURE);
-          }
-          // Accumulate events values
-          for (int i = 0; i < NEVENTS; i++)
-          {
-            trsm2[omp_get_thread_num()].values[i] += values[i];
-          }
-          trsm2[omp_get_thread_num()].values[NEVENTS]++;
-        }
-        else if (TPM_TRACE)
-        {
-          gettimeofday(&end, NULL);
-          // TPM library: send time and name
-          tpm_upstream_get_task_time(start, end, name_with_id_char);
-        }
-      }
-
-      for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
-      {
-        double *tileA = A(m, k);
-        double *tileB = A(k, n);
-        double *tileC = A(m, n);
-
-        if (TPM_TRACE)
-        {
-          // TPM library: create a unique task name
-          name_with_id_char = tpm_unique_task_identifier("gemm", k, m, n);
-          tpm_upstream_set_task_name(name_with_id_char);
-        }
-
-#pragma omp task depend(in : tileA[0 : A.tile_size * A.tile_size], \
-                            tileB[0 : A.tile_size * A.tile_size])  \
-    depend(inout : tileC[0 : A.tile_size * A.tile_size])
-        {
-          if (TPM_PAPI)
-          {
-            memset(values, 0, sizeof(values));
-            // Start PAPI counters
-            int ret_start = PAPI_start(eventset);
-            if (ret_start != PAPI_OK)
+            tpm_tile_to_matrix(A + k * tile_size * matrix_size + k * tile_size * tile_size,
+                               pA + k * tile_size * matrix_size + k * tile_size, m, tile_size,
+                               tile_size, matrix_size);
+            LAPACKE_dgetrf(LAPACK_COL_MAJOR, m, tile_size, pA + k * tile_size * matrix_size + k * tile_size,
+                           matrix_size, ipiv + k * tile_size);
+            // Update the ipiv
+            for (int i = k * tile_size; i < k * tile_size + tile_size; i++)
             {
-              printf("PAPI_start GEMM error %d: %s\n", ret_start, PAPI_strerror(ret_start));
-              exit(EXIT_FAILURE);
+                ipiv[i] += k * tile_size;
             }
-          }
-          else if (TPM_TRACE)
-          {
-            // TPM library: send CPU and name
-            unsigned int cpu, node;
-            getcpu(&cpu, &node);
-            tpm_upstream_set_task_cpu_node(cpu, node, name_with_id_char);
-            gettimeofday(&start, NULL);
-          }
-
-          // Kernel
-          cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, A.tile_size,
-                      A.tile_size, A.tile_size, -1.0, tileA, A.tile_size, tileB,
-                      A.tile_size, 1.0, tileC, A.tile_size);
-
-          if (TPM_PAPI)
-          {
-            // Start PAPI counters
-            int ret_stop = PAPI_stop(eventset, values);
-            if (ret_stop != PAPI_OK)
-            {
-              printf("PAPI_stop GEMM error %d: %s\n", ret_stop, PAPI_strerror(ret_stop));
-              exit(EXIT_FAILURE);
-            }
-            // Accumulate events values
-            for (int i = 0; i < NEVENTS; i++)
-            {
-              gemm[omp_get_thread_num()].values[i] += values[i];
-            }
-            gemm[omp_get_thread_num()].values[NEVENTS]++;
-          }
-          else if (TPM_TRACE)
-          {
-            gettimeofday(&end, NULL);
-            // TPM library: send time and name
-            tpm_upstream_get_task_time(start, end, name_with_id_char);
-          }
+            tpm_matrix_to_tile(A + k * tile_size * matrix_size + k * tile_size * tile_size,
+                               pA + k * tile_size * matrix_size + k * tile_size, m, tile_size,
+                               tile_size, matrix_size);
         }
-      }
+
+        // Update trailing submatrix
+        for (int j = k + 1; j < matrix_size / tile_size; j++)
+        {
+            double *akj = A + j * tile_size * matrix_size + k * tile_size * tile_size;
+
+#pragma omp task depend(in : akk[0 : m * tile_size]) \
+    depend(in : ipiv[k * tile_size : tile_size])     \
+    depend(inout : akj[0 : tile_size * tile_size])   \
+    firstprivate(akk, akj, m)
+            {
+                int k1 = k * tile_size;
+                int k2 = k * tile_size + tile_size;
+                tpm_geswp(A + j * tile_size * matrix_size, tile_size, k1, k2, ipiv);
+
+                cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, tile_size,
+                            tile_size, alpha, akk, tile_size, akj, tile_size);
+
+                for (int i = k + 1; i < matrix_size / tile_size; i++)
+                {
+                    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, tile_size, tile_size,
+                                tile_size, neg,
+                                A + k * tile_size * matrix_size + i * tile_size * tile_size,
+                                tile_size, akj, tile_size, alpha,
+                                A + j * tile_size * matrix_size + i * tile_size * tile_size, tile_size);
+                }
+            }
+        }
     }
-  }
+    // Pivoting to the left
+    for (int t = 1; t < matrix_size / tile_size; t++)
+    {
+#pragma omp task depend(in : ipiv[((matrix_size / tile_size) - 1) * tile_size : tile_size]) \
+    depend(inout : A[t * tile_size * matrix_size : matrix_size * tile_size])
+        {
+            tpm_geswp(A + (t - 1) * tile_size * matrix_size, tile_size, t * tile_size, matrix_size, ipiv);
+        }
+    }
+    tpm_tile_to_matrix(A, pA, matrix_size, matrix_size, tile_size, matrix_size);
 
-  if (TPM_PAPI)
-  {
-#pragma omp taskwait
-    PAPI_destroy_eventset(&eventset);
-    PAPI_shutdown();
-
-    const char *task_names[] = {"getrf", "trsm1", "trsm2", "gemm"};
-    CounterData *counters[] = {getrf, trsm1, trsm2, gemm};
-    int num_tasks = sizeof(task_names) / sizeof(task_names[0]); // This gives the length of the tasks array
-    dump_counters("lu", task_names, counters, num_tasks, A.matrix_size, A.tile_size, l3_cache_size, available_threads);
-  }
+#ifdef LOG
+    tpm_default_print_matrix("A", pA, matrix_size);
+#endif
 }
