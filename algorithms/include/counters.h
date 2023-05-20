4ca201a3a92df3e096e1b7db531d26bb8f940b89
Idriss Daoudi <idaoudi@anl.gov>
2023-05-20 02:16:01 +0200

    [feature] output raw PAPI counters and manipulate them later

--- algorithms/include/counters.h
+++ algorithms/include/counters.h
@@ -22,20 +22,24 @@ typedef struct
 {
     // NEVENTS + 1 for the task weight
     long long values[NEVENTS + 1];
-    double arithm_intensity;
-    double mem_boundness;
-    double bmr;
-    double ilp;
+    long long papi_l3_tcm;
+    long long papi_tot_ins;
+    long long papi_res_stl;
+    long long papi_tot_cyc;
+    long long papi_br_msp;
+    long long papi_br_ins;
     int weight;
 } CounterData;
 
 void compute_derived_metrics(CounterData *data)
 {
     // PAPI_L3_TCM, PAPI_TOT_INS, PAPI_RES_STL, PAPI_TOT_CYC, PAPI_BR_MSP, PAPI_BR_INS
-    data->arithm_intensity = (double)data->values[0] / (double)data->values[1];
-    data->mem_boundness = (double)data->values[2] / (double)data->values[3];
-    data->bmr = (double)data->values[4] / (double)data->values[5];
-    data->ilp = (double)data->values[1] / (double)data->values[3];
+    data->papi_l3_tcm = data->values[0];
+    data->papi_tot_ins = data->values[1];
+    data->papi_res_stl = data->values[2];
+    data->papi_tot_cyc = data->values[3];
+    data->papi_br_msp = data->values[4];
+    data->papi_br_ins = data->values[5];
     data->weight = (int)data->values[6];
 }
 
@@ -85,14 +89,14 @@ void dump_counters(const char *algorithm, const char *task_names[], CounterData 
         int first_char = fgetc(file);
         if (first_char == EOF)
         {
-            fprintf(file, "algorithm,task,matrix_size,tile_size,frequency,mem_boundness,arithm_intensity,bmr,ilp,l3_cache_ratio,weight\n");
+            fprintf(file, "algorithm,task,matrix_size,tile_size,frequency,papi_l3_tcm,papi_tot_ins,papi_res_stl,papi_tot_cyc,papi_br_msp,papi_br_ins,l3_cache_size,weight\n");
         }
 
         for (int i = 0; i < num_tasks; i++)
         {
-            fprintf(file, "%s,%s,%d,%d,%d,%f,%f,%f,%f,%f,%d\n", algorithm, task_names[i], matrix_size, tile_size, TPM_PAPI_FREQ,
-                    total_counters[i].mem_boundness, total_counters[i].arithm_intensity, total_counters[i].bmr, total_counters[i].ilp,
-                    (double)total_counters[i].values[0] / (double)l3_cache_size, total_counters[i].weight);
+            fprintf(file, "%s,%s,%d,%d,%d,%lld,%lld,%lld,%lld,%lld,%lld,%f,%d\n", algorithm, task_names[i], matrix_size, tile_size, TPM_PAPI_FREQ,
+                    total_counters[i].papi_l3_tcm, total_counters[i].papi_tot_ins, total_counters[i].papi_res_stl, total_counters[i].papi_tot_cyc, total_counters[i].papi_br_msp, total_counters[i].papi_br_ins,
+                    l3_cache_size, total_counters[i].weight);
         }
 
         fclose(file);
