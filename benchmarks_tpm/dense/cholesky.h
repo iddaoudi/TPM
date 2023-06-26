f223cb4717bf445fd052ff959df34debc9744ee8
Idriss Daoudi <idaoudi@anl.gov>
2023-06-26 14:58:01 -0500

    [fix] major error in Cholesky

--- benchmarks_tpm/dense/cholesky.h
+++ benchmarks_tpm/dense/cholesky.h
@@ -4,6 +4,7 @@ void cholesky(tpm_desc A)
 
   TPM_application_start();
   double time_start = omp_get_wtime();
+  printf("Time start %f\n", time_start);
 
   int k = 0, m = 0, n = 0;
 
@@ -19,61 +20,61 @@ depend(inout : tileA[0 : A.tile_size * A.tile_size])
       LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'U', A.tile_size, tileA, A.tile_size);
 
       TPM_application_task_finish("potrf");
+    }
 
-      for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
-      {
-        double *tileA = A(k, k);
-        double *tileB = A(k, m);
+    for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
+    {
+      double *tileA = A(k, k);
+      double *tileB = A(k, m);
 
 #pragma omp task                                  \
 depend(in : tileA[0 : A.tile_size * A.tile_size]) \
     depend(inout : tileB[0 : A.tile_size * A.tile_size])
-        {
-          TPM_application_task_start("trsm");
+      {
+        TPM_application_task_start("trsm");
 
-          cblas_dtrsm(CblasColMajor, CblasLeft, CblasUpper, CblasTrans,
-                      CblasNonUnit, A.tile_size, A.tile_size, 1.0, tileA,
-                      A.tile_size, tileB, A.tile_size);
+        cblas_dtrsm(CblasColMajor, CblasLeft, CblasUpper, CblasTrans,
+                    CblasNonUnit, A.tile_size, A.tile_size, 1.0, tileA,
+                    A.tile_size, tileB, A.tile_size);
 
-          TPM_application_task_finish("trsm");
-        }
+        TPM_application_task_finish("trsm");
+      }
+    }
+
+    for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
+    {
+      double *tileA = A(k, m);
+      double *tileB = A(m, m);
+
+#pragma omp task                                  \
+depend(in : tileA[0 : A.tile_size * A.tile_size]) \
+    depend(inout : tileB[0 : A.tile_size * A.tile_size])
+      {
+        TPM_application_task_start("syrk");
+        cblas_dsyrk(CblasColMajor, CblasUpper, CblasTrans, A.tile_size,
+                    A.tile_size, -1.0, tileA, A.tile_size, 1.0, tileB,
+                    A.tile_size);
+        TPM_application_task_finish("syrk");
       }
 
-      for (m = k + 1; m < A.matrix_size / A.tile_size; m++)
+      for (n = k + 1; n < m; n++)
       {
-        double *tileA = A(k, m);
-        double *tileB = A(m, m);
-
-#pragma omp task                                  \
-depend(in : tileA[0 : A.tile_size * A.tile_size]) \
-    depend(inout : tileB[0 : A.tile_size * A.tile_size])
-        {
-          TPM_application_task_start("syrk");
-          cblas_dsyrk(CblasColMajor, CblasUpper, CblasTrans, A.tile_size,
-                      A.tile_size, -1.0, tileA, A.tile_size, 1.0, tileB,
-                      A.tile_size);
-          TPM_application_task_finish("syrk");
-        }
-
-        for (n = k + 1; n < m; n++)
-        {
-          double *tileA = A(k, n);
-          double *tileB = A(k, m);
-          double *tileC = A(n, m);
+        double *tileA = A(k, n);
+        double *tileB = A(k, m);
+        double *tileC = A(n, m);
 
 #pragma omp task                                  \
 depend(in : tileA[0 : A.tile_size * A.tile_size], \
            tileB[0 : A.tile_size * A.tile_size])  \
     depend(inout : tileC[0 : A.tile_size * A.tile_size])
-          {
-            TPM_application_task_start("gemm");
+        {
+          TPM_application_task_start("gemm");
 
-            cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, A.tile_size,
-                        A.tile_size, A.tile_size, -1.0, tileA, A.tile_size, tileB,
-                        A.tile_size, 1.0, tileC, A.tile_size);
+          cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, A.tile_size,
+                      A.tile_size, A.tile_size, -1.0, tileA, A.tile_size, tileB,
+                      A.tile_size, 1.0, tileC, A.tile_size);
 
-            TPM_application_task_finish("gemm");
-          }
+          TPM_application_task_finish("gemm");
         }
       }
     }
@@ -82,5 +83,7 @@ depend(in : tileA[0 : A.tile_size * A.tile_size], \
 #pragma omp taskwait
 
   double time_finish = omp_get_wtime();
+  printf("Time finish %f\n", time_finish);
+  printf("Time total %f\n", time_finish - time_start);
   TPM_application_finalize(time_finish - time_start);
 }
